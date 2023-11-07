900be2a328ca81d5de6587d38824f64c76bc11ed
Nayemul <rsnaimul4@gmail.com>
2023-11-08 03:20:56 +0600

    First Commit

---
+++ product/templatetags/category.py
@@ -0,0 +1,11 @@
+# from django import template
+# from product.models import Catagory
+
+# register = template.library()
+
+
+# @register.filter(name='Category')
+# def category(user):
+#     if user.is_authenticated:
+#         cat = category.objects.filter(parent=None)
+#         return cat
